560026f5da0069199e2f9cc134ea832fc7a9ba1d
Moises Cordero <moises.cordero.smb@gmail.com>
2026-08-28 08:23:39 -0500

    Agrego la calculadora de radio

--- circunferencia.php
+++ circunferencia.php
@@ -5,7 +5,7 @@
 <body>
     <h1>Calcular el area de una circunferencia</h1>
 
-    <form method="post" action="resultado.php">
+    <form method="post" action="resultado_radio.php">
         Ingrese el radio de la circunferencia:
         <input type="text" name="radio" id="radio">
         <br><br>
